7070745e9931cbf49f55af60893dc2ac46c0a14e
geminopa <genshi32@icloud.com>
2023-02-20 23:17:21 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
+import Name from './components/Name';
+import Message from './components/Message';
+import Parent from './components/ContainerSample';
+import Page from './components/ContextSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
+  // document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
